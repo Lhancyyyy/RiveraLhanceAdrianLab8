6c821a03c52de4ff0d2d28da571e6ff393ec6226
lhancyyyy <lhanceadrian.rivera.iics@ust.edu.ph>
2018-12-20 17:20:27 +0800

    Initial commit

---
+++ app/src/main/java/com/rivera/lhanceadrian/Record.java
@@ -0,0 +1,29 @@
+package com.rivera.lhanceadrian;
+
+public class Record {
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public Record(String fname, String gender, int age) {
+
+        this.fname = fname;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    String fname;
+    String gender;
+    int age;
+
+
+}
